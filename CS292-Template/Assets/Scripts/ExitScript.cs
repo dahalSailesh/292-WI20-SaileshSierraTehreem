d2e19b42d4dc00e35b4e8ca63db339b68ca739b5
Sailesh Dahal <sdahal@knox.edu>
2020-02-04 15:53:22 -0600

    Only the Scripts

---
+++ CS292-Template/Assets/Scripts/ExitScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitScript : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void ClosePanel(){
+        if(Panel != null){
+            Panel.SetActive(false);
+        }
+    }
+}
---
+++ CS292-Template/Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] hazards;
+    float startTimeBtwSpawns = 1.25f;
+
+    private float timeBtwSpawns;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeBtwSpawns <= 0){
+            Transform randomSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+            GameObject randomHazard = hazards[Random.Range(0, hazards.Length)];
+
+            Instantiate(randomHazard, randomSpawnPoint.position, Quaternion.identity);
+            timeBtwSpawns = startTimeBtwSpawns;
+        }
+        else{
+            timeBtwSpawns -= Time.deltaTime;
+        }
+    }
+}
