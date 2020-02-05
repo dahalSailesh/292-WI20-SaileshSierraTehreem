aea24dd7a69c51be86b21ce712e9436658fb6b15
s <susyverud@knox.edu>
2020-02-04 21:10:22 -0600

    curr game

---
+++ CS292-Template/Assets/Scripts/truckMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class truckMove : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float speed = 15.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = rb.position;
+        position.x = position.x - 3.0f * speed * Time.deltaTime;
+        rb.MovePosition(position);
+    }
+}
