6375498ee83d56b19e049c6cba4ebc94e03dca53
s <susyverud@knox.edu>
2020-02-09 17:03:55 -0600

    sun aft noon

---
+++ CS292-Template/Assets/Scripts/PauseButtonScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseButtonScript : MonoBehaviour
+{
+    public GameObject Panel;
+    public GameObject Panel2;
+
+    public static bool GameIsPaused = false;
+    public static bool GameOver = false;
+
+    public void OpenPanel()
+    {
+        if (Panel != null)
+        {
+            Panel.SetActive(true);
+        }
+    }
+    public void Resume()
+    {
+        Panel.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        Panel.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Panel.SetActive(false);
+        Panel2.SetActive(true);
+        Debug.Log("Loading menu...");
+        GameOver = true;
+        // Application.LoadLevel(0);
+        SceneManager.LoadScene("temp-w-gamescreen");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
--- CS292-Template/Assets/Scripts/Timer.cs
+++ CS292-Template/Assets/Scripts/Timer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,7 +7,8 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     float currentTime = 0f;
-    float startingTime = 30f;
+    float startingTime = 5f;
+    public GameObject Panel;
 
     [SerializeField] Text countdownText;
 
@@ -17,7 +19,24 @@ public class Timer : MonoBehaviour
 
     void Update()
     {
+        if (PauseButtonScript.GameOver)
+        {
+            currentTime = 0;
+            countdownText.text = currentTime.ToString("0");
+            Application.LoadLevel(0);
+            return;
+        }
+        if (Convert.ToInt32(currentTime) == 0)
+        {
+            GameOver();
+            Panel.SetActive(true);
+        }
         currentTime -= 1 * Time.deltaTime;
         countdownText.text = currentTime.ToString("0");
+    }
+
+    void GameOver()
+    {
+        Time.timeScale = 0f;
     }
 }
